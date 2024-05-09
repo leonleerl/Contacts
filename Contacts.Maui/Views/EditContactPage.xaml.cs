8a134d72268e17dc6c16a2e1b46e1b65c8b52fb2
locodurian <leon.runlongli@gmail.com>
2024-05-09 13:55:40 +0800

    add Control

--- Contacts.Maui/Views/EditContactPage.xaml.cs
+++ Contacts.Maui/Views/EditContactPage.xaml.cs
@@ -25,36 +25,30 @@ public partial class EditContactPage : ContentPage
             if (contact != null)
             {
                 // lblName.Text = contact.Name;
-                entryName.Text = contact.Name;
-                entryEmail.Text = contact.Email;
-                entryAddress.Text = contact.Address;
-                entryPhone.Text = contact.Phone;
+                contactCtrl.Name = contact.Name;
+                contactCtrl.Email = contact.Email;
+                contactCtrl.Address = contact.Address;
+                contactCtrl.Phone = contact.Phone;
             }
         }
     }
 
     private void btnUpdate_Clicked(object sender, EventArgs e)
     {
-        if (nameValidator.IsNotValid)
-        {
-            DisplayAlert("Error", "Name is required", "OK");
-            return;
-        }
 
-        if (emailValidator.IsNotValid)
-        {
-            foreach(var error in emailValidator.Errors)
-            {
-                DisplayAlert("Error", error.ToString(), "Ok");
-            }
-            return;
-        }
 
-        contact.Name = entryName.Text;
-        contact.Address = entryAddress.Text;
-        contact.Phone = entryPhone.Text;
-        contact.Email = entryEmail.Text;
+        contact.Name = contactCtrl.Name;
+        contact.Address = contactCtrl.Address;
+        contact.Phone = contactCtrl.Phone;
+        contact.Email = contactCtrl.Email;
         ContactRepository.UpdateContact(contact.ContactId, contact);
         Shell.Current.GoToAsync("..");
+    }
+
+
+    private void contactCtrl_OnError(object sender, string e)
+    {
+        DisplayAlert("Error", e, "OK");
+
     }
 }
